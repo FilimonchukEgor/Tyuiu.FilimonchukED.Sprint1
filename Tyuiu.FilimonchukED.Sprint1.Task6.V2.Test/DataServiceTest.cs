df1610f55181c8bebae407da45470920677018bd
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-10 21:41:22 +0500

    Task6

---
+++ Tyuiu.FilimonchukED.Sprint1.Task6.V2.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.FilimonchukED.Sprint1.Task6.V2.Lib;
+namespace Tyuiu.FilimonchukED.Sprint1.Task6.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            string strtest = "asdaoksojdasoHelloasdgdaha";
+            var res = ds.CheckHello(strtest);
+            Assert.IsTrue(res);
+        }
+    }
+}
